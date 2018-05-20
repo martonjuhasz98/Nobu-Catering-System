061a17fd8570281ee1f2853f12288126693c11c8
flawlessBadger <jozefferko9@gmail.com>
2018-05-20 14:50:31 +0200

    merge

--- src/guilayer/analytics/SalesTab.java
+++ src/guilayer/analytics/SalesTab.java
@@ -62,7 +62,7 @@ public class SalesTab extends JPanel {
 		toDatePicker.setBounds(195, 0, 150, 30);
 		add(toDatePicker);
 
-		JComboBox filterBox = new JComboBox(ic.getCategories());
+		JComboBox filterBox = new JComboBox(ic.getCategories().toArray());
 		filterBox.setBounds(360, 0, 150, 30);
 		add(filterBox);
 
